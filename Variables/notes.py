956fea89e7c638540a6f59a481a0a992f6b5f1ef
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-01-29 09:52:41 -0700

    python practice

--- Variables/notes.py
+++ Variables/notes.py
@@ -1,1 +1,6 @@
 # James Wilcox, Variables Notes
+
+name = input("Please tell me your name: ")
+
+print("I don't wanna backslide to where i've started from")
+print("There's no chance I will shake this again")
---
+++ Variables/practice.py
@@ -0,0 +1,13 @@
+# James Wilcox, Variables Practice Python
+
+name = "James"
+smalnum = "43.2"
+bignum = "432"
+food = "fried_egg"
+color = "purple"
+scholnme = "Utah_County_Academy_of_Sciences"
+year = "2025 CE"
+eyeclr = "blue"
+age = "14"
+favsubj = "math"
+
