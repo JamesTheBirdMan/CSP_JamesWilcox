1db518fb36b2a8eaa543f30c1041967a8e6fb754
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-01-30 09:20:22 -0700

    practice python

--- Variables/practice.py
+++ Variables/practice.py
@@ -1,13 +1,20 @@
 # James Wilcox, Variables Practice Python
 
 name = "James"
-smalnum = "43.2"
+smalnum = "3"
 bignum = "432"
 food = "fried_egg"
 color = "purple"
 scholnme = "Utah_County_Academy_of_Sciences"
-year = "2025 CE"
+year = "2025_Common_Era"
 eyeclr = "blue"
 age = "14"
 favsubj = "math"
 
+print("my name is", name)
+print("a small number is", smalnum, "and a big number is", bignum)
+print("for breakfast I had a", food)
+print("my favorite color is", color, "and I go to", scholnme)
+print("the year is", year)
+print("my eyes are", eyeclr, "and I am", age, "years old")
+print("my favorite subject is", favsubj)
