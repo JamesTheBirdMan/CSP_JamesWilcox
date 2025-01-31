29fb709339eea606ea8b068288a6fb640cc3ef48
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-01-31 09:06:30 -0700

    practice c

---
+++ Variables/practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char name[] = "James";
+char subject[] = "math";
+char eye[] = "blue";
+char school[] = "UCAS";
+char color[] = "purple";
+char food[] = "toast";
+int smlnum = 42;
+int bignum = 424;
+int year = 2025;
+int age = 14;
+
+int main(void){
+    printf(" %s\n", name);
+    printf(" %s\n", subject);
+    printf(" %s\n", eye);
+    printf(" %s\n", school);
+    printf(" %s\n",color);
+    printf(" %s\n", food);
+    printf("%d\n", bignum);
+    printf("%d\n", smlnum);
+    printf("%d\n", year);
+    printf("%d\n", age);
+    return 0;
+}
