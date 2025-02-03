2c84a4c220532c8baa6872313c05a0c83383fb71
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-03 09:22:06 -0700

    fixed template for c

---
+++ Variables/notes.c
@@ -0,0 +1,13 @@
+// James Wilcox, Variables Notes
+#include <stdio.h>
+
+char name[] = "James";
+int num = 42;
+float pi = 3.1415926535;
+
+int main(void){
+    printf(" %s\n", name);
+    printf("%d\n", num);
+    printf("%f\n", pi);
+    return 0;
+}
--- template.c
+++ template.c
@@ -1,12 +1,7 @@
+// James Wilcox, Template
 #include <stdio.h>
 
-char name[] = "James";
-int num = 42;
-float pi = 3.1415926535;
-
 int main(void){
-    printf(" %s\n", name);
-    printf("%d\n", num);
-    printf("%f\n", pi);
+printf("Hello World"); 
     return 0;
 }
