49116f2a174d03ce9dd79b5e706a7d9d4b2e686b
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-05 09:52:08 -0700

    working on c expressions

--- expressions/practice.c
+++ expressions/practice.c
@@ -1,7 +1,17 @@
 // James Wilcox, Expressions Practice
 #include <stdio.h>
+#include <math.h>
+
+int par1 = (7-24/8*4+6);
+int par2 = (18/3-7+2*5);
+int par3 = (6*4/12+72/8-9);
+int par4_1 = (17-6/2);
+int par4_2 = (4*3);
 
 int main(void){
-printf("Hello World"); 
+printf("%d\n", par1);
+printf("%d\n", par2);
+printf("%d\n", par3);
+printf("%d\n", par4_1 + par4_2);
     return 0;
 }
