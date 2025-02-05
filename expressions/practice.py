41ac3e526b4cd294fcaef6deba0aba87a53219f4
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-05 09:41:46 -0700

    expressions py done

--- expressions/practice.py
+++ expressions/practice.py
@@ -1,8 +1,19 @@
 # James Wilcox, Expressions Practice
 
-print(7-24/8*4+6)
-print(18/3-7+2*5)
-print(6*4/12+72/8-9)
-print((17-6/2)+4*3)
-print(-2(1*4-2/2)+(6+2-3))
-print(-1*((3-4*7)/5)-2*24/6)
+print(7-24/8*4+6) #problem1
+print(18/3-7+2*5) #problem2
+print(6*4/12+72/8-9) #problem3
+equ41 = (17-6/2) #problem4
+print(equ41+4*3)#problem4
+equ51 = (1*4-2/2) #problem5
+equ52 = (6+2-3) #problem5
+print(- 2*equ51+equ52) #problem5
+print(-1*((3-4*7)/5)-2*24/6) #problem6
+equ71 = (3*5**2/15) #problem7
+equ72 = (5-2**2) #problem7
+print(equ71 - equ72) #problem7
+equ81 = (1**4*2**2+3**3) #problem8
+print(equ81-2**5/4) #problem8
+equ91 = (22/2-2*5) #problem9
+equ92 = (4-6/6) #problem9
+print(equ91**2+equ92**2) #problem9
