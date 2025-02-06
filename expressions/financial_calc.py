f475adc1fdd11de06635e0a1a464fd9a43989d9d
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-06 09:27:25 -0700

    cleaned up files

---
+++ expressions/financial_calc.py
@@ -0,0 +1,1 @@
+# James Wilcox, Financial Calculator Python
--- expressions/practice.c
+++ expressions/practice.c
@@ -7,11 +7,20 @@ int par2 = (18/3-7+2*5);
 int par3 = (6*4/12+72/8-9);
 int par4_1 = (17-6/2);
 int par4_2 = (4*3);
+int par5_1 = (1*4-2/2);
+int par5_2 = (6+2-3);
+int par6_1 = (3-4*7);
+int par6_2 = (-2*24/6);
+int par7_1 = (3*5**2/15);
+int par7_2 = (5-2**2);
 
 int main(void){
 printf("%d\n", par1);
 printf("%d\n", par2);
 printf("%d\n", par3);
 printf("%d\n", par4_1 + par4_2);
+printf("%d\n", -2*(par5_1) + par5_2);
+printf("%d\n", -1*par6_1/5 - par6_2);
+printf("%d\n", par7_1 - par7_2);
     return 0;
 }
