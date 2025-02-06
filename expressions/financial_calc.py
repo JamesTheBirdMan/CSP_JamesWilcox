09635e6104e70be4bf0847818e286c00164e70d8
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-06 09:48:40 -0700

    set up for financial calc

--- expressions/financial_calc.py
+++ expressions/financial_calc.py
@@ -1,1 +1,40 @@
 # James Wilcox, Financial Calculator Python
+
+# Write a print statement telling the user what the program is (Budget Calculator)
+
+# Ask for monthly income (User input)
+income =float(input("what is your monthly income\n"))
+
+# Ask for monthly rent (User Input)
+
+# Ask for monthly utilites (User Input)
+
+# Ask for monthly groceries (User Input)
+
+# Ask for monthly transportation (User Input)
+
+# Calculate savings as 10% of income
+
+# Calculate spending money imncome - (rent+utilities+groceries+transportation+savings) (variable)
+
+# Calculate percent of rent (rent/income) (variable
+
+# Calculate percent of utilities (utilities/income) (variable)
+
+# Calculate percent of groceries (groceries/income) (variable)
+
+# Calculate percent of transportation (transportation/income) (variable)
+
+# Calculate percent of spending (spending/income (variable)
+
+# Tell user catgory spending amount and percent for rent ("You spend $XX.XX on rent and that is XX% of your income")
+
+# Tell user catgory spending amount and percent for utilities("You spend $XX.XX on groceries and that is XX% of your income")
+
+# Tell user catgory spending amount and percent for groceries("You spend $XX.XX on groceries and that is XX% of your income")
+
+# Tell user catgory spending amount and percent for transportation("You spend $XX.XX on transportation and that is XX% of your income")
+
+# Tell user catgory spending amount and percent for savings ("You spend $XX.XX on savings and that is XX% of your income")
+
+# Tell user catgory spending amount and percent for spending ("You spend $XX.XX on spending and that is XX% of your income")
--- expressions/practice.c
+++ expressions/practice.c
@@ -11,8 +11,8 @@ int par5_1 = (1*4-2/2);
 int par5_2 = (6+2-3);
 int par6_1 = (3-4*7);
 int par6_2 = (-2*24/6);
-int par7_1 = (3*5**2/15);
-int par7_2 = (5-2**2);
+int par7_1 = pow(5, 2);
+int par7_2 = pow(2, 2)
 
 int main(void){
 printf("%d\n", par1);
@@ -21,6 +21,5 @@ printf("%d\n", par3);
 printf("%d\n", par4_1 + par4_2);
 printf("%d\n", -2*(par5_1) + par5_2);
 printf("%d\n", -1*par6_1/5 - par6_2);
-printf("%d\n", par7_1 - par7_2);
     return 0;
 }
