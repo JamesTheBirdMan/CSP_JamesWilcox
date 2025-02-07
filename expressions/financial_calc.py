74cd42fc05dc052ba86df48b741e0883835e6c41
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-07 09:12:28 -0700

    finish c expressions

--- expressions/financial_calc.py
+++ expressions/financial_calc.py
@@ -15,17 +15,17 @@ income =float(input("what is your monthly income\n"))
 
 # Calculate savings as 10% of income
 
-# Calculate spending money imncome - (rent+utilities+groceries+transportation+savings) (variable)
+# Calculate spending money income - (rent+utilities+groceries+transportation+savings) (variable)
 
-# Calculate percent of rent (rent/income) (variable
+# Calculate percent of rent (rent/income)*100 (variable
 
-# Calculate percent of utilities (utilities/income) (variable)
+# Calculate percent of utilities (utilities/income)*100 (variable)
 
-# Calculate percent of groceries (groceries/income) (variable)
+# Calculate percent of groceries (groceries/income)*100 (variable)
 
-# Calculate percent of transportation (transportation/income) (variable)
+# Calculate percent of transportation (transportation/income)*100 (variable)
 
-# Calculate percent of spending (spending/income (variable)
+# Calculate percent of spending (spending/income (variable)*100
 
 # Tell user catgory spending amount and percent for rent ("You spend $XX.XX on rent and that is XX% of your income")
 
