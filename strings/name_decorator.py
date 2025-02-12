3b72c3a91810ab8f71dde864f2c8ee62b137e0fe
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-12 09:29:02 -0700

    finished name decoration

--- strings/name_decorator.py
+++ strings/name_decorator.py
@@ -1,1 +1,6 @@
 # James Wilcox, Name Decorator Python
+
+name = input("What is your name? ").strip().capitalize()
+name_deco1 = "~:O-|-{ "
+name_deco2 = " }-|-O:~"
+print(f"hello {name_deco1}{name}{name_deco2} welcome to my program.\n")
