72ddab8658778fe4beb850bf2a0c166546c51a9f
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-12 09:50:00 -0700

    name decorator for real finished

--- strings/name_decorator.py
+++ strings/name_decorator.py
@@ -3,4 +3,5 @@
 name = input("What is your name? ").strip().capitalize()
 name_deco1 = "~:O-|-{ "
 name_deco2 = " }-|-O:~"
-print(f"hello {name_deco1}{name}{name_deco2} welcome to my program.\n")
+full_name = name_deco1+name+name_deco2
+print(f"hello {full_name}welcome to my program.\n")
