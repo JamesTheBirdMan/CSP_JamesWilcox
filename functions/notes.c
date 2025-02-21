5921e1f373829d202ebe32c3f0dbeae30474f6b2
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-21 09:52:15 -0700

    FINALLY DONE WITH HELLOW ROLD C

--- functions/notes.c
+++ functions/notes.c
@@ -1,23 +1,29 @@
 // James Wilcox, Functions Notes C
 #include <stdio.h>
+#include <string.h>
+char asnwer[20];
+char first[20];
+char second[20];
+char third[20];
+char fourth[20];
+char fifth[20];
 
-void add(int num1, int num2){
-    printf("%d\n", num1+num2);
-}
-
-char input(char type[20]){
-    char answer[50];
-    printf("Give me a %s:\n", type);
-    scanf("%s", answer);
-    return answer;
+char* hellow(int count){
+printf("Tell me person #%d's name:\n", count);
+scanf("%s", asnwer);
+return asnwer;
 }
 
 int main(void){
-   //printf("Hello World\n");
-   //add(12, 9);
-   input("name");
-   input("verb");
-   input("place");
-   printf("%s was %s until they somehow reached %s", word("name"), word("verb"), word("place"));
+strcpy(first, hellow(1));  
+printf("It's nice to meet you, %s!\n", first);
+strcpy(second, hellow(2)); 
+printf("It's nice to meet you, %s and  %s!\n", second, first); 
+strcpy(third, hellow(3));  
+printf("It's nice to meet you, %s, %s, and %s!\n", third, second, first);
+strcpy(fourth, hellow(4));  
+printf("It's nice to meet you, %s, %s, %s, and %s!\n", fourth, third, second, first);
+strcpy(fifth, hellow(5));  
+printf("It's nice to meet you, %s, %s, %s, %s, and %s!\n", fifth, fourth, third, second, first);
     return 0;
 }
