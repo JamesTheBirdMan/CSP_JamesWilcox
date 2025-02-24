7d37f1927d3a6187910b075b65f2a34ba9d7a46f
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-02-24 08:58:14 -0700

    hello world c done

--- functions/hello_world.c
+++ functions/hello_world.c
@@ -1,17 +1,30 @@
 // James Wilcox, Update Hello World C
+// James Wilcox, Functions Notes C
 #include <stdio.h>
 #include <string.h>
+char asnwer[20];
+char first[20];
+char second[20];
+char third[20];
+char fourth[20];
+char fifth[20];
 
-char answer[20];
-char name1[20];
-char name2[20];
-char name3[20];
-char name4[20];
-char name5[20];
+char* hellow(int count){
+printf("Tell me person #%d's name:\n", count);
+scanf("%s", asnwer);
+return asnwer;
+}
 
-char hello(int number){
-    for (int cond = 0; cond<5;cond + 1);{
-        printf("Tell me person %s's name:\n", number);
-    }
+int main(void){
+strcpy(first, hellow(1));  
+printf("It's nice to meet you, %s!\n", first);
+strcpy(second, hellow(2)); 
+printf("It's nice to meet you, %s and  %s!\n", second, first); 
+strcpy(third, hellow(3));  
+printf("It's nice to meet you, %s, %s, and %s!\n", third, second, first);
+strcpy(fourth, hellow(4));  
+printf("It's nice to meet you, %s, %s, %s, and %s!\n", fourth, third, second, first);
+strcpy(fifth, hellow(5));  
+printf("It's nice to meet you, %s, %s, %s, %s, and %s!\n", fifth, fourth, third, second, first);
     return 0;
 }
