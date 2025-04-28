d3fde92f8b4fcfeacf10c4deb344be86ff2bce4c
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-04-28 09:52:20 -0600

    hmhmhmhmhmmhmhhmmh

--- Final Project/final.js
+++ Final Project/final.js
@@ -1,16 +1,12 @@
-function show(){
-    document.getElementById("extinfo").style.visibility = "visible";
+function show() {
+    document.getElementById("extinfo").style.display = "block";
     document.getElementById("infbtn").style.display = "none";
+    document.getElementById("lesbtn").style.visibility = "visible";
     document.getElementById("lesbtn").style.display = "inline";
-    document.getElementById("lesbtn").style.textAlign = "center";
 }
 
-function hide(){
-    document.getElementById("extinfo").style.visibility = "hidden";
+function hide() {
+    document.getElementById("extinfo").style.display = "none";
     document.getElementById("infbtn").style.display = "inline";
     document.getElementById("lesbtn").style.display = "none";
-}
-
-function showpic{
-    document.getElementById("delcpic").style.display= "inline";
 }
