a670d982746acf3f6599517ae0555a6f823308c2
JamesTheBirdMan <james.wilcox@ucas-edu.net>
2025-05-01 09:38:54 -0600

    fgdf

--- Final Project/final.js
+++ Final Project/final.js
@@ -1,12 +1,16 @@
-function show() {
+function show(){
     document.getElementById("extinfo").style.display = "block";
     document.getElementById("infbtn").style.display = "none";
     document.getElementById("lesbtn").style.visibility = "visible";
     document.getElementById("lesbtn").style.display = "inline";
 }
 
-function hide() {
+function hide(){
     document.getElementById("extinfo").style.display = "none";
     document.getElementById("infbtn").style.display = "inline";
     document.getElementById("lesbtn").style.display = "none";
+}
+
+function link(){
+    window.open("https://bit.ly/4jW5zfd")
 }
